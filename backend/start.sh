3bb3309c7611c3cbea914a9bf623779d12fa2f3c
Ecoimbra1990 <ankecs@gmail.com>
2025-08-05 19:48:00 -0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -8,9 +8,9 @@ pip install -r requirements.txt
 # Coleta os arquivos estáticos
 python manage.py collectstatic --no-input
 
-# Executa as migrações do banco de dados.
-# Com uma base de dados nova, este comando irá criar todas as tabelas do zero.
-python manage.py migrate
+# Executa as migrações. A flag '--fake-initial' resolve o erro de
+# "tabela já existe" ao fazer o deploy pela primeira vez ou após um reset.
+python manage.py migrate --fake-initial
 
 # Cria o superusuário inicial (se não existir)
 python manage.py create_initial_superuser
