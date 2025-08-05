a98182712bae3ce0d2bc5bc3f0e27ea19073d4d8
Ecoimbra1990 <ankecs@gmail.com>
2025-08-05 17:28:58 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
