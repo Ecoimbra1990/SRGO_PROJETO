81fc2b8d3a4e02e00457bdd8eb53147728e1274f
Ecoimbra1990 <ankecs@gmail.com>
2025-08-04 21:24:55 -0300

    template

--- backend/srgo/settings.py
+++ backend/srgo/settings.py
@@ -40,7 +40,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'srgo.urls'
 
 TEMPLATES = [
-    # ... (o resto da configuração de TEMPLATES permanece o mesmo) ...
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'srgo.wsgi.application'
