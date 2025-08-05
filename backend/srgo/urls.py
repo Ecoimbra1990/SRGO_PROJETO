5a56371822e13d350b98d0d0a14561e7424f2fb7
Ecoimbra1990 <ankecs@gmail.com>
2025-08-04 21:13:22 -0300

    update urls

--- backend/srgo/urls.py
+++ backend/srgo/urls.py
@@ -1,10 +1,12 @@
 # Arquivo: backend/srgo/urls.py
 
 from django.contrib import admin
-from django.urls import path, include  # Garanta que 'include' está importado
+from django.urls import path, include
 
 urlpatterns = [
+    # Esta linha é a que ativa o painel de administração
     path('admin/', admin.site.urls),
-    # Conecta todas as URLs do nosso app de ocorrências ao prefixo /api/
+
+    # Esta linha conecta as URLs da sua aplicação de ocorrências
     path('api/', include('ocorrencias.urls')),
 ]
