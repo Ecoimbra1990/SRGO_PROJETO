181ddbd9ace511f6e95abaf7b4ca9ebe6d102d8f
Ecoimbra1990 <ankecs@gmail.com>
2025-08-05 17:02:33 -0300

    Update views.py

--- backend/ocorrencias/views.py
+++ backend/ocorrencias/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, generics
 from .models import Ocorrencia
 from .serializers import OcorrenciaSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from django.contrib.auth.models import User # <-- IMPORTAÇÃO ADICIONADA
+from django.contrib.auth.models import User
 
 # View para registrar um novo usuário
 class UserCreate(generics.CreateAPIView):
@@ -15,6 +15,6 @@ class OcorrenciaViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows ocorrencias to be viewed or edited.
     """
-    queryset = Ocorrencia.objects.all().order_by('-data_ocorrencia')
+    queryset = Ocorrencia.objects.all().order_by('-data_criacao') # CORREÇÃO APLICADA AQUI
     serializer_class = OcorrenciaSerializer
     permission_classes = [IsAuthenticated] # Exige autenticação para acessar
