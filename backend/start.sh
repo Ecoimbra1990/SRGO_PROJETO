c4be874493c16958e48955512df36a8f7e90d144
Ecoimbra1990 <ankecs@gmail.com>
2025-08-06 07:25:10 -0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -2,28 +2,24 @@
 # exit on error
 set -o errexit
 
-# Instala as dependências
 pip install -r requirements.txt
-
-# Coleta os arquivos estáticos
 python manage.py collectstatic --no-input
 
-# --- Bloco de Recuperação da Base de Dados ---
-# 1. Força a limpeza das tabelas da app para evitar o erro "already exists".
 echo "--- Limpando tabelas da app 'ocorrencias' ---"
 python manage.py reset_app_db
 
-# 2. Redefine o histórico de migrações da app para o estado zero.
 echo "--- Redefinindo o histórico de migrações ---"
 python manage.py migrate ocorrencias zero --fake
 
-# 3. Executa as migrações normalmente. Agora, o Django irá recriar as tabelas do zero.
 echo "--- Aplicando migrações ---"
 python manage.py migrate
 
-# 4. Cria o superusuário (se não existir)
+echo "--- Criando superusuário ---"
 python manage.py create_initial_superuser
 
-# 5. Inicia o servidor web
+# (NOVO) Popula a tabela Efetivo a partir do Google Sheets
+echo "--- Populando tabela Efetivo ---"
+python manage.py populate_efetivo
+
 echo "--- Iniciando o servidor Gunicorn ---"
 gunicorn srgo.wsgi
