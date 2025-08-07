0db88e51f7a18c84f9b39584496c10e71dac0656
Ecoimbra1990 <ankecs@gmail.com>
2025-08-07 14:44:48 -0300

    Update admin.py

--- backend/ocorrencias/admin.py
+++ backend/ocorrencias/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Ocorrencia, PessoaEnvolvida, ProcedimentoPenal, OrganizacaoCriminosa, TipoOcorrencia, CadernoInformativo, Efetivo
+from .models import (
+    Ocorrencia, PessoaEnvolvida, ProcedimentoPenal, 
+    OrganizacaoCriminosa, TipoOcorrencia, CadernoInformativo, 
+    Efetivo, OPM, ModeloArma, ArmaApreendida
+)
 
 admin.site.register(Ocorrencia)
 admin.site.register(PessoaEnvolvida)
@@ -8,3 +12,6 @@ admin.site.register(OrganizacaoCriminosa)
 admin.site.register(TipoOcorrencia)
 admin.site.register(CadernoInformativo)
 admin.site.register(Efetivo)
+admin.site.register(OPM)
+admin.site.register(ModeloArma)
+admin.site.register(ArmaApreendida)
