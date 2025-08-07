1b6d94a9737e95cbaf0de37fe47dc1fa5ea3534f
Ecoimbra1990 <ankecs@gmail.com>
2025-08-07 14:44:02 -0300

    Update urls.py

--- backend/ocorrencias/urls.py
+++ backend/ocorrencias/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import *
+from .views import (
+    OcorrenciaViewSet, 
+    OrganizacaoCriminosaViewSet, 
+    TipoOcorrenciaViewSet,
+    CadernoInformativoViewSet,
+    OPMViewSet,
+    ModeloArmaViewSet
+)
 
 router = DefaultRouter()
 router.register(r'ocorrencias', OcorrenciaViewSet, basename='ocorrencia')
@@ -8,7 +15,7 @@ router.register(r'organizacoes', OrganizacaoCriminosaViewSet, basename='organiza
 router.register(r'tipos-ocorrencia', TipoOcorrenciaViewSet, basename='tipoocorrencia')
 router.register(r'cadernos', CadernoInformativoViewSet, basename='caderno')
 router.register(r'opms', OPMViewSet, basename='opm')
-router.register(r'modelos-arma', ModeloArmaViewSet, basename='modeloarma') # <-- Adicionar esta linha
+router.register(r'modelos-arma', ModeloArmaViewSet, basename='modeloarma')
 
 urlpatterns = [
     path('', include(router.urls)),
