6051894e1a9b9eb888805c4639310114a71f60b5
Ecoimbra1990 <ankecs@gmail.com>
2025-08-07 11:47:54 -0300

    Update urls.py

--- backend/srgo/urls.py
+++ backend/srgo/urls.py
@@ -1,22 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    OcorrenciaViewSet, 
-    UserCreate, 
-    OrganizacaoCriminosaViewSet, 
-    TipoOcorrenciaViewSet,
-    CadernoInformativoViewSet,
-    OPMViewSet # Importe a OPMViewSet
-)
+# backend/srgo/urls.py
 
-router = DefaultRouter()
-router.register(r'ocorrencias', OcorrenciaViewSet)
-router.register(r'organizacoes', OrganizacaoCriminosaViewSet)
-router.register(r'tipos-ocorrencia', TipoOcorrenciaViewSet)
-router.register(r'cadernos', CadernoInformativoViewSet)
-router.register(r'opms', OPMViewSet) # Adicione o registro da rota
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register/', UserCreate.as_view(), name='user_create'),
+    path('api/', include('ocorrencias.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
