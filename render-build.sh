cb5ce3f4f413200847284d93edd0808ed8dc4cb8
Ecoimbra1990 <ankecs@gmail.com>
2025-08-08 10:47:00 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,11 +2,22 @@
 # exit on error
 set -o errexit
 
+# --- Build do Backend ---
+echo "--- Iniciando o build do Backend ---"
+cd backend
+
+# Instala as dependências do Python
+pip install -r requirements.txt
+
+# Coleta os arquivos estáticos
+python manage.py collectstatic --no-input
+cd ..
+
 # --- Build do Frontend ---
 echo "--- Iniciando o build do Frontend ---"
 cd frontend
 
-# (NOVO) Limpa o cache e instala as dependências do zero
+# Limpa o cache e instala as dependências
 echo "--- Limpando o cache e reinstalando as dependências ---"
 npm cache clean --force
 rm -rf node_modules
@@ -15,13 +26,3 @@ npm install
 # Constrói a aplicação
 echo "--- Construindo a aplicação React ---"
 npm run build
-
-# --- Build do Backend ---
-echo "--- Iniciando o build do Backend ---"
-cd ../backend
-
-# Instala as dependências do Python
-pip install -r requirements.txt
-
-# Coleta os arquivos estáticos
-python manage.py collectstatic --no-input
