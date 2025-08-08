6289e47e6cfe001d1e619d3fa08d0babe5eadf8a
Ecoimbra1990 <ankecs@gmail.com>
2025-08-08 18:02:37 -0300

    Update admin.py

--- backend/ocorrencias/admin.py
+++ backend/ocorrencias/admin.py
@@ -3,15 +3,13 @@ from .models import (
     Ocorrencia, PessoaEnvolvida, ProcedimentoPenal, 
     OrganizacaoCriminosa, TipoOcorrencia, CadernoInformativo, 
     Efetivo, OPM, ModeloArma, ArmaApreendida,
-    RISP, AISP, Localidade, ModalidadeCrime # Importa o novo modelo
+    RISP, AISP, Localidade, ModalidadeCrime
 )
 
 class OcorrenciaAdmin(admin.ModelAdmin):
-    # ... (código existente da OcorrenciaAdmin - sem alterações)
     list_display = ('id', 'tipo_ocorrencia', 'data_fato', 'cidade', 'bairro', 'opm_area')
     search_fields = ('id', 'descricao_fato', 'cidade', 'bairro')
     list_filter = ('data_fato', 'tipo_ocorrencia', 'opm_area')
-    
     fields = [
         'tipo_ocorrencia', 'tipo_homicidio', 'caderno_informativo', 'data_fato', 
         'descricao_fato', 'evolucao_ocorrencia', 'fonte_informacao', 'foto_ocorrencia',
@@ -33,6 +31,4 @@ admin.site.register(ArmaApreendida)
 admin.site.register(RISP)
 admin.site.register(AISP)
 admin.site.register(Localidade)
-
-# --- REGISTO DO NOVO MODELO ---
 admin.site.register(ModalidadeCrime)
