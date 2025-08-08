a0a0406c64e2c03a9ecff68a6b38d86a4699ee9f
Ecoimbra1990 <ankecs@gmail.com>
2025-08-08 08:13:44 -0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -11,10 +11,10 @@ python manage.py migrate
 
 # Os comandos abaixo só precisam ser executados uma vez ou quando os dados mudam.
 # Comentá-los evita problemas de timeout durante o deploy.
- python manage.py create_initial_superuser
- python manage.py populate_efetivo
- python manage.py populate_armas
- python manage.py populate_areas
+ #python manage.py create_initial_superuser
+ #python manage.py populate_efetivo
+ #python manage.py populate_armas
+ #python manage.py populate_areas
 
 # Inicia o servidor web
 gunicorn srgo.wsgi:application --bind 0.0.0.0:$PORT
